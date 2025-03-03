c75b4af3c5724b867107794b25b8c4acf6aaa5ea
Abhishek Verma <abhishekverma6533@gmail.com>
2025-03-04 01:12:06 +0530

    App.Jsx page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage/HomePage";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import "./App.css";
 
-function App() {
-
+const App = () => {
   return (
-    <>
-       
-    </>
-  )
-}
+    <Router>
+      <div className="app-container">
+        {/* SEO Optimized Meta Tags */}
+        <meta name="description" content="VedaWiki - The Ultimate Encyclopedia on Indian Knowledge & History" />
+        <meta name="keywords" content="VedaWiki, Indian Encyclopedia, History, Knowledge" />
+        <meta name="author" content="Abhishek Verma" />
+        
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
